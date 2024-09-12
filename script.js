30bb2f3785c54719c28d92417a0d19f34f870a53
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-09-12 19:54:59 +0530

    changes

--- script.js
+++ script.js
@@ -14,20 +14,20 @@ var video3 = document.querySelector(".video2")
 
 
 
-var circle1 = document.querySelector(".circle1")
-var circle2 = document.querySelector(".circle2")
-
+var LightDarkMode = document.querySelector(".LightDarkMode")
 var body = document.querySelector("body")
 
 
 
 CARTIER.addEventListener("mouseenter",function(){
   video1.classList.remove("hidden")
+  body.style.color = "white";
     
 })
 
 CARTIER.addEventListener("mouseleave",function(){
     video1.classList.add("hidden")
+    body.style.color = "black";
       
   })
 
@@ -35,38 +35,39 @@ CARTIER.addEventListener("mouseleave",function(){
 
   URWERK.addEventListener("mouseenter",function(){
     video2.classList.remove("hidden")
+    body.style.color = "white";
       
 })
 
 
 URWERK.addEventListener("mouseleave",function(){
     video2.classList.add("hidden")
+    body.style.color = "black";
       
   })
 
 RIOT.addEventListener("mouseenter",function(){
     video3.classList.remove("hidden")
+    body.style.color = "white";
       
 })
 
 RIOT.addEventListener("mouseleave",function(){
     video3.classList.add("hidden")
+    body.style.color = "black";
       
   })
 
 
-
-  circle1.addEventListener("click",function(){
-    body.style.color="white"
-    body.style.backgroundColor="black"
-
-
-    
-  })
-
-  circle1.addEventListener("dblclick",function(){
-    body.style.color="black"
-    body.style.backgroundColor="white"
-    
-    
-  })
+  var flag = 0;
+  LightDarkMode.addEventListener("click", function() {
+  if (flag === 0) {
+    body.style.color = "white";
+    body.style.backgroundColor = "black";
+    flag = 1;
+  } else {
+    body.style.color = "black";
+    body.style.backgroundColor = "white";
+    flag = 0;
+  }
+});
